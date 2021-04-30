d4762b37e7991b6a3b955b1e33a789036aaab475
Paulo H. Marchi <marchipauloh@gmail.com>
2021-04-30 15:57:54 -0300

    Ajustando alpine.sh com versoes recentes

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,11 +1,10 @@
-sudo su
-echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && \
-echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositories
+echo "http://dl-4.alpinelinux.org/alpine/latest-stable/main" >> /etc/apk/repositories && \
+echo "http://dl-4.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
 
 apk update && \
 	apk add build-base \
     libxml2-dev \
     libxslt-dev \
-    postgresql-dev \
+    freetds-dev \
     curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
     && rm -rf /var/cache/apk/*
